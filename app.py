0127700e971b5ae4ac2a5fe645e4ad1938f1b0c0
Suma <suma@Kaushiks-MacBook-Air.local>
2025-04-14 13:32:09 -0500

    Fixed NameError in admin_dashboard by importing defaultdict from collections

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 import logging
 import os
+from collections import defaultdict  # Add this import
 
 app = Flask(__name__)
 
